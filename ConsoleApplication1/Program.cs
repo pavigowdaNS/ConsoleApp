6c9c54dfdf17b88026005fbf6a5f0d8dfc2cb65b
pavigowdaNS <pavi1988gowda@gmail.com>
2018-03-06 16:44:08 +0530

    fdvf
    bvc bvbv

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+            Console.WriteLine(a.i);
+            B b = new B();
+            b.i();
+            Console.ReadLine();
+        }
+    }
+   public class A
+    {
+        public string i = "a";
+
+    }
+    public class B:A
+    {
+        public new void i() { Console.WriteLine("b"); }
+    }
+}
